24f3f149a0fe6bc42a798237d75533726b8b60da
Avinash <avinashvootla@gmail.com>
2020-08-03 16:38:26 +0530

    Home screen

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Login from './login/Login'
-
+import Home from './home/Home'
 
 
 class Controller extends Component {
@@ -15,7 +15,8 @@ class Controller extends Component {
             <Router>
                 {/* Route to login screen */}
                 <Route exact path='/' render={(props) => <Login {...props} />} />
-               
+                {/* Route to home screen */}
+                <Route exact path='/home' render={(props) => <Home {...props} baseUrl={this.baseUrl} />} />
             </Router>
         )
     }
